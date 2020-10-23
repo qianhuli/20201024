deb69f05357ef13faf58679a0caedf75d3e9e79e
huhu <1970172781@qq.com>
2020-10-24 00:38:20 +0800

    init project

---
+++ 20201024git/20201024git/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  20201024git
+//
+//  Created by huli on 2020/10/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
